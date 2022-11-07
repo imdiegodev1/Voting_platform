9517e50b30fdf9a3d954b5a70653fff37fa2f53b
imdiegodev1 <diegoanr40@hotmail.com>
2022-11-06 21:06:16 -0500

    voting app recovery - django practice backend

---
+++ votingapp/polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    #Ex: /polls/
+    ##path("", views.index, name="index"), ##Based on functions
+    path("", views.IndexView.as_view(), name="index"),  ##Based on classes
+    #Ex: /polls/5
+    ##path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/detail", views.DetailView.as_view(), name="detail"),
+    #Ex: /polls/5/results
+    ##path("<int:question_id>/results>", views.results, name="results"),
+    path("<int:pk>/results>", views.ResultsView.as_view(), name="results"),
+    #Ex: /polls/5/votes
+    path("<int:question_id>/votes>", views.vote, name="vote"),
+]
